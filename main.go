1bb15fd4fba5ab80820e213be10a55e6bb6e25fd
Ivan Zhutyaev <gitivanzhutyaev@gmail.com>
2026-09-13 11:25:50 +0300

    formatted output

--- main.go
+++ main.go
@@ -7,4 +7,11 @@ func main() { // Создание функции
 	var andrey string                         // Объявление
 	andrey = "andrey"                         // Присвоение
 	fmt.Println("Hello World", svyat, andrey) // Вывод
+
+	var str = "hello"
+	var intval = 78
+	var floatval = 56.34233
+	var boolval = true
+	fmt.Printf("%v, %v, %e, %t", str, intval, floatval, boolval)
+
 }
